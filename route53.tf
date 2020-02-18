0ba9fbd0d1e811f0c0aa2dce59be94bcea04c483
andrewdavies7780 <59688323+andrewdavies7780@users.noreply.github.com>
2020-02-18 09:44:52 +0000

    Create route53.tf
    added creation of route 53 records

---
+++ route53.tf
@@ -0,0 +1,18 @@
+resource "aws_route53_zone" "main" {
+  name = "dekker.and.digital"
+}
+output "main_name_servers" {
+  value = aws_route53_zone.main.name_servers
+}
+resource "aws_route53_record" "main-txt" {
+  zone_id = aws_route53_zone.main.id
+  name = "andy.dekker.and.digital"
+  type = "TXT"
+  records = [
+    ns-1731.awsdns-24.co.uk.
+    ns-599.awsdns-10.net.
+  ns-247.awsdns-30.com.
+  ns-1440.awsdns-52.org.
+  ]
+  ttl = 900
+}
