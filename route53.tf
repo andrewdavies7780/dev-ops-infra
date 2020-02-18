57816e32db33518e99a64b6ccd620b449a41db80
andrewdavies7780 <59688323+andrewdavies7780@users.noreply.github.com>
2020-02-18 10:39:04 +0000

    Update route53.tf
    updated tf script

--- route53.tf
+++ route53.tf
@@ -1,18 +1,35 @@
-resource "aws_route53_zone" "main" {
-  name = "dekker.and.digital"
+variable "dns" {
+  default = "aa.dekker-and.digital"
 }
+
+resource "aws_route53_zone" "main" {
+  name = var.dns
+}
+
 output "main_name_servers" {
   value = aws_route53_zone.main.name_servers
 }
+
 resource "aws_route53_record" "main-txt" {
   zone_id = aws_route53_zone.main.id
-  name = "andy.dekker.and.digital"
+  name = var.dns
   type = "TXT"
+  records = [
+    "test=abc", # more text records can be added below
+  ]
+  ttl = 900
+}
+
+
+resource "aws_route53_record" "andy-zone" {
+  name = "andy.${var.dns}"
+  zone_id = aws_route53_zone.main.id
+  type = "NS"
+  ttl = 900
   records = [
     "ns-1731.awsdns-24.co.uk",
     "ns-599.awsdns-10.net",
     "ns-247.awsdns-30.com",
     "ns-1440.awsdns-52.org"
   ]
-  ttl = 900
 }
